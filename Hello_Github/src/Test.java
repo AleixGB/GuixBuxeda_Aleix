2863f4017731da74421617730cf1ca9b142c6d70
Aleix Guix <Aleix@Aleix-PC>
2014-01-23 19:40:03 +0100

    2n Commit: He afegit un bucle for que conta fins a 10 i quan arriva a 10 diu adeu.

--- Hello_Github/src/Test.java
+++ Hello_Github/src/Test.java
@@ -3,8 +3,15 @@ public class Test {
 
 	public static void main(String[] args) {
 	
-		System.out.println("Hello World!!! \nHello Github!!");
+		System.out.println("Hello World!!! \nHello Github!!\n");
 		
+		
+		for(int i = 1; i <= 10; i++){
+			System.out.println(i);
+			if(i == 10){
+				System.out.println("\nAdeu");
+			}
+		}
 	}
 
 }
